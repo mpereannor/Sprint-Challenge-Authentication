a991a3cead429ec482131aaa8b66bde75cc77fb5
chudnofskyATD <mpereannor@gmail.com>
2019-11-15 06:29:34 +0000

    helper functions created, register endpoint implemented and tested successfully

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/users-model');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+  .then(saved => { 
+    res.status(201).json(saved);
+  })
+  .catch(error => { 
+    res.status(500).json(error)
+  });
 });
 
 router.post('/login', (req, res) => {

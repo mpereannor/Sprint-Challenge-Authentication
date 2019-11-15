503cd7c13085bd4aaa71bdd8818a17f12e009422
chudnofskyATD <mpereannor@gmail.com>
2019-11-15 09:51:11 +0000

    helper function add tested successfully

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -1,8 +1,24 @@
 const Users = require('./users-model');
 const db = require('../database/dbConfig');
 
-beforeEach( async () => { 
-    await db('users').truncate()
-})
+// beforeEach( async () => { 
+//     await db('users').truncate()
+// })
 
-describe('Users database', () => { })
+describe('Users database', () => { 
+    describe('insert function', () => { 
+        let users
+        test( 'should insert a user', async () => { 
+            await Users.add({
+                username: 'timothy',
+                password: 'bro'})
+                
+            await Users.add({
+                username:'luke',
+                password: 'bruh'})
+
+            users = await db('users')
+            expect(users).toHaveLength(2)
+        })
+    })
+})
